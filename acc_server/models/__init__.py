28d45d0f7e339fbb5264bab4d36c7aa818ce1a1a
Буренин Артём <burenotti@gmail.com>
2021-02-28 00:07:54 +0300

    serverWorkerSettings added to __init__

--- acc_server/models/__init__.py
+++ acc_server/models/__init__.py
@@ -1,2 +1,2 @@
 from .server_settings_related_models import ServerSettings, EventRules, EventSettings, AssistRules, Session, Event, \
-    Track, ServerConfig, Playlist
+    Track, ServerConfig, Playlist, ServerWorkerSettings
