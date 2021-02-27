2b54fabf1f37288dc815059da71a944a34b6cd28
Буренин Артём <burenotti@gmail.com>
2021-02-27 19:26:54 +0300

    Event: added reference for additional tables

--- acc_server/models/__init__.py
+++ acc_server/models/__init__.py
@@ -1,1 +1,2 @@
-from .server_settings_related_models import ServerSettings, EventRules, EventSettings, AssistRules, Session
+from .server_settings_related_models import ServerSettings, EventRules, EventSettings, AssistRules, Session, Event, \
+    Track, ServerConfig, Playlist
--- acc_server/models/server_settings_related_models.py
+++ acc_server/models/server_settings_related_models.py
@@ -1,6 +1,8 @@
 from django.db import models
-from django.db.models import ForeignKey, IntegerField, CharField, BooleanField, FloatField, TextField, DateTimeField
+from django.db.models import ForeignKey, IntegerField, CharField, BooleanField, FloatField, TextField, DateTimeField, OneToOneField
 from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models import signals
 
 
 class Track(models.Model):
@@ -13,7 +15,7 @@ class Track(models.Model):
 class EventSettings(models.Model):
 
     id = IntegerField(primary_key=True)
-    track = ForeignKey(Track, models.deletion.CASCADE)
+    track = ForeignKey(Track, models.deletion.CASCADE, null=True)
     pre_race_waiting_time_seconds = IntegerField(default=60)
     session_over_time_seconds = IntegerField(default=120)
     ambient_temp = IntegerField(default=26)
@@ -33,7 +35,6 @@ class Session(models.Model):
     time_multiplier = IntegerField(null=False, default=0)
     session_type = IntegerField(null=False, default=0)
     session_duration_minutes = IntegerField(null=False)
-    event = ForeignKey(EventSettings, on_delete=models.deletion.CASCADE)
 
 
 class EventRules(models.Model):
@@ -44,7 +45,7 @@ class EventRules(models.Model):
     driver_stint_time_sec = IntegerField(null=False, default=-1)
     mandatory_pitstop_count = IntegerField(null=False, default=0)
     max_total_driving_time = IntegerField(null=False, default=-1)
-    max_drivers_count = IntegerField(null=False)
+    max_drivers_count = IntegerField(null=False, default=100)
     is_refuelling_allowed_in_race = BooleanField(default=True)
     is_refuelling_time_fixed = BooleanField(default=False)
     is_mandatory_pitstop_refuelling_required = BooleanField(default=False)
@@ -89,9 +90,9 @@ class ServerSettings(models.Model):
 
 class ServerConfig(models.Model):
     id = IntegerField(primary_key=True)
-    udp_port = IntegerField(unique=True)
-    tcp_port = IntegerField(unique=True)
-    max_connections = IntegerField()
+    udp_port = IntegerField(default=0)
+    tcp_port = IntegerField(default=0)
+    max_connections = IntegerField(default=100)
     lan_discovery = IntegerField(default=0)
     register_to_lobby = IntegerField(default=1)
 
@@ -108,4 +109,27 @@ class Event(models.Model):
     name = CharField(max_length=255)
     starts_at = DateTimeField()
     playlist = ForeignKey(Playlist, models.deletion.CASCADE)
+    server_settings = ForeignKey(ServerSettings, models.deletion.RESTRICT, null=True)
+    server_config = ForeignKey(ServerConfig, models.deletion.RESTRICT, null=True)
+    assist_rules = ForeignKey(AssistRules, models.deletion.RESTRICT, null=True)
+    event_settings = ForeignKey(EventSettings, models.deletion.RESTRICT, null=True)
+    event_rules = ForeignKey(EventRules, models.deletion.RESTRICT, null=True)
     # TODO: Дописать поля
+
+
+@receiver(signals.pre_save, sender=Event)
+def create_event_related_settings(sender, instance: Event, **kwargs):
+    instance.event_rules = EventRules.objects.create()
+    instance.assist_rules = AssistRules.objects.create()
+    instance.event_settings = EventSettings.objects.create()
+    instance.server_config = ServerConfig.objects.create()
+    instance.server_settings = ServerSettings.objects.create()
+
+
+@receiver(signals.pre_save, sender=Event)
+def save_event_related_settings(sender, instance: Event, **kwargs):
+    instance.server_config.save()
+    instance.server_settings.save()
+    instance.assist_rules.save()
+    instance.event_settings.save()
+    instance.event_rules.save()
