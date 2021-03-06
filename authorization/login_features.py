2015b650166101d417e864a6b92811e46d4a15c3
zaqbez39me <korolov_k@mail.ru>
2021-03-06 16:20:36 +0300

    Login page edited

---
+++ authorization/login_features.py
@@ -0,0 +1,32 @@
+from django.contrib import messages
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.models import User
+
+from authorization.serializers import RegisterSerializer
+
+
+def check_anonymous(user):
+    if not user.is_anonymous:
+        return user
+    else:
+        return None
+
+
+def register_proj(username, request, content, form):
+    if not User.objects.filter(username=username).exists():
+        serializer = RegisterSerializer.create(RegisterSerializer(), form.data)
+        user = serializer.save()
+        user = authenticate(username=form.data['username'], password=form.data['password'])
+        login_proj(user, request, content, form)
+    else:
+        messages.add_message(request, messages.ERROR, f'This username is already taken: {username}')
+
+
+def login_proj(user, request, content, form):
+    if user is not None:
+        login(request, user)
+        content.update({'authorized': True, 'user': user, 'login_form': form})
+    else:
+        messages.add_message(request, messages.WARNING, f'Incorrect username or password')
+        content.update({'user': user, 'login_form': form})
+    return content
--- authorization/views.py
+++ authorization/views.py
@@ -1,15 +1,13 @@
 from django.contrib.admin import register
-from django.contrib.auth.models import User, AnonymousUser
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render
 from django.contrib import messages
 from django.core.validators import validate_email
 from . import serializers
-
 # Create your views here.
 
 from authorization.forms import LoggingInForm, RegistrationForm
-from .serializers import RegisterSerializer
+from .login_features import check_anonymous, register_proj, login_proj
 
 
 def index(request):
@@ -21,43 +19,24 @@ def index(request):
             logout(request)
     return render(request, 'pages/index.html', content)
 
-def check_Anonymous(user):
-    if not user.is_anonymous:
-        return user
-    else:
-        return None
 
 def log_in(request):
-    content = dict()
-    register()
-    content['login_form'] = LoggingInForm()
-    content['register_form'] = RegistrationForm()
-    content['user'] = request.user
+    content = {'login_form': LoggingInForm(), 'register_form': RegistrationForm(), 'user': request.user}
     if request.method == "POST":
         post = request.POST
         if 'logout' in post:
             logout(request)
+
         elif 'logging' in post:
             form = LoggingInForm(request.POST)
             user = authenticate(username=form.data['username'], password=form.data['password'])
-            if user is not None:
-                login(request, user)
-                content['authorized'] = True
-                content['user'] = user
-                content['login_form'] = form
-            else:
-                messages.add_message(request, messages.WARNING, f'Incorrect username or password')
+            content = login_proj(user, request, content, form)
+
         elif 'register' in post:
             form = RegistrationForm(request.POST)
             if form.is_valid():
                 username = form.data['username']
-                if not User.objects.filter(username=username).exists():
-                    serializer = RegisterSerializer.create(RegisterSerializer(), form.data)
-                    for elem in form.data:
-                        print(elem)
-                    user = serializer.save()
-                else:
-                    messages.add_message(request, messages.ERROR, f'This username is already taken: {username}')
+                register_proj(username, request, content, form)
             else:
                 output_fields = list()
                 for a in RegistrationForm().base_fields:
@@ -65,20 +44,18 @@ def log_in(request):
                 for elem in form.data:
                     if len(form.data[elem]) == 0 and elem != 'register':
                         messages.add_message(request, messages.ERROR, f'The {elem} field is not correct')
+
                     elif elem == 'email':
                         try:
                             validate_email(form.data[elem])
                         except Exception as e:
                             messages.add_message(request, messages.ERROR, str(e.args[0]))
+
                     if elem != 'csrfmiddlewaretoken':
                         output_fields.remove(str(elem))
                 for field in output_fields:
                     messages.add_message(request, messages.ERROR, f'The {field} field is not correct')
             content['register_form'] = form
-        content['request'] = request
-        content['user'] = check_Anonymous(request.user)
-        return render(request, 'pages/login.html', content)
-    elif request.method == "GET":
-        content['request'] = request
-        content['user'] = check_Anonymous(request.user)
-        return render(request, 'pages/login.html', content)
+
+    content.update({'request': request, 'user': check_anonymous(request.user)})
+    return render(request, 'pages/login.html', content)
