df6249fe08c51eef8810578f57601ebb063ad814
zaqbez39me <korolov_k@mail.ru>
2021-03-27 20:02:04 +0300

    Added login.js

---
+++ authorization/templates/scripts/login.js
@@ -0,0 +1,28 @@
+var progressBarValue = 100;
+var progressBarsTime = setInterval(progressBars, 15 * document.getElementsByClassName('messages').length);
+
+function progressBars() {
+    if (progressBarValue < 0) {
+        hider()
+        clearInterval(progressBarsTime);
+    } else {
+        $('.progress-login').css({
+            width: progressBarValue + '%'
+        });
+
+        $('.progress-register').css({
+            width: progressBarValue + '%'
+        });
+        progressBarValue--;
+    }
+}
+
+function hider() {
+    $('.messages-block').hide(600, function () {
+        $('.messages-block').remove();
+    });
+}
+
+function choose_tab(a){
+      document.getElementById(a).click();
+}
