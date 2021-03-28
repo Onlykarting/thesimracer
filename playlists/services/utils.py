3a1d1dbf1f6c8a63d87ccbbaf5ad65da4abd16b1
Буренин Артём <burenotti@gmail.com>
2021-03-28 16:24:31 +0300

    get_event_if_available возвращает ивент, если он не скрыт или его пользователь является создателем

--- playlists/services/utils.py
+++ playlists/services/utils.py
@@ -8,7 +8,12 @@ def get_recent_events(limit: int = 50, offset: int = 0, user: User = None):
 
 
 def get_event_if_available(user: User, event_id: int):
-    event = Event.objects.filter(id=event_id)
-    if len(event):
-        return event
+    event_list = Event.objects.filter(id=event_id)
+    if len(event_list):
+        event = event_list[0]
+        if event.playlist.hidden:
+            if user.is_authenticated and user.id == event.playlist.creator:
+                return event
+        else:
+            return event
     return None
