0dae37507ebecef5444b5c36e2adc9a012639961
Артём Буренин <burenotti@gmail.com>
2021-04-03 15:12:44 +0300

    admin register model

--- playlists/admin.py
+++ playlists/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from playlists.models import Playlist, Event, AccEvent
 
-# Register your models here.
+
+class EventAdmin(admin.ModelAdmin):
+    pass
+
+
+class PlaylistAdmin(admin.ModelAdmin):
+    pass
+
+
+class AccEventAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Event, EventAdmin)
+admin.site.register(Playlist, PlaylistAdmin)
