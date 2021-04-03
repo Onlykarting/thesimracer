3f02496eb357a575bf120de173f4bb6f21ab418b
zaqbez39me <korolov_k@mail.ru>
2021-04-03 12:21:25 +0300

    new-pages

--- playlists/urls.py
+++ playlists/urls.py
@@ -1,8 +1,14 @@
-from .views import event, get_events, create_playlist, get_playlist
+from .views import event, get_events, create_playlist, get_playlist, get_results, indev
 from django.urls import path
 
 urlpatterns = [
     path('eventlist/', get_events),
+    path('resultlist/', get_results),
+    path('tools/', indev),
+    path('reports/', indev),
+    path('support/', indev),
+    path('stats/', indev),
+    path('help-us/', indev),
     path('event/<int:event_id>', event),
     path('playlist/create', create_playlist),
     path('playlist/<int:playlist_id>', get_playlist)
