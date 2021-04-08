bbbb78f7eb1444c3fa7839d8c3bcd911fe8e5b96
zaqbez39me <korolov_k@mail.ru>
2021-04-08 22:05:40 +0300

    added event-page

--- playlists/services/utils.py
+++ playlists/services/utils.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from playlists.models import Event, Playlist
+from json import dumps
 
 
 def get_recent_events(limit: int = 50, offset: int = 0, user: User = None):
@@ -9,11 +10,13 @@ def get_recent_events(limit: int = 50, offset: int = 0, user: User = None):
 
 def get_event_if_available(user: User, event_id: int):
     event_list = Event.objects.filter(id=event_id)
-    if len(event_list):
-        event = event_list[0]
-        if event.playlist.hidden:
-            if user.is_authenticated and user.id == event.playlist.creator:
-                return event
+    event__ = [entry for entry in event_list.values()]
+    if len(event__) > 0:
+        event__ = event__[0]
+    if len(event__) != 0:
+        event = event__
+        if user.is_authenticated:
+            return event
         else:
             return event
     return None
