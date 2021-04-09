c0759c3d17b05bb4662e30ae35cedf1cd7d224ef
Артём Буренин <burenotti@gmail.com>
2021-04-09 17:58:11 +0300

    Добавлен отбор только официальных ивентов

--- playlists/services/utils.py
+++ playlists/services/utils.py
@@ -3,23 +3,23 @@ from playlists.models import Event, Playlist
 from json import dumps
 
 
-def get_recent_events(limit: int = 50, offset: int = 0, user: User = None):
-    events = Event.objects.order_by('starts_at')[offset: offset + limit]
-    return events
+def get_recent_events(limit: int = 50, offset: int = 0, user: User = None, verified=True):
+    if verified:
+        events = Event.objects.filter(is_verified=True)
+    else:
+        events = Event.objects
+    return events.order_by('starts_at')[offset: offset + limit]
 
 
-def get_event_if_available(user: User, event_id: int):
+def get_event_if_available(user: User, event_id: int) -> Event:
     event_list = Event.objects.filter(id=event_id)
-    event__ = [entry for entry in event_list.values()]
-    if len(event__) > 0:
-        event__ = event__[0]
-    if len(event__) != 0:
-        event = event__
-        if user.is_authenticated:
-            return event
+    if len(event_list):
+        event = event_list[0]
+        if event.playlist.hidden:
+            if user.is_authenticated and user.id == event.playlist.creator:
+                return event
         else:
             return event
-    return None
 
 
 def get_playlist_if_available(user: User, playlist_id: int):
