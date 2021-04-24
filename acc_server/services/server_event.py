5696e3231364b58cdc3386f1202ec39bc81dcc1f
Артём Буренин <burenotti@gmail.com>
2021-04-24 12:06:08 +0300

    Полностью переписаны ивенты: теперь они являются датаклассами и не отвечают за парсинг.

--- acc_server/services/server_event.py
+++ acc_server/services/server_event.py
@@ -1,81 +1,97 @@
 import re
 import subprocess
+from dataclasses import dataclass, field
 from .server_worker_proxy import ServerWorkerProxy
-from .utils import try_parse_int
 
 
+@dataclass(frozen=True)
 class ServerEvent:
-
-    pattern = re.compile(r'(?P<timestamp>\d+):\s+')
-
-    def __init__(self,
-                 raw_string: str = None,
-                 worker: ServerWorkerProxy = None,
-                 out_stream_type: int = subprocess.STDOUT):
-        self.out_stream_type = out_stream_type
-        self.worker = worker
-        self.raw_string = raw_string
-        self.timestamp: int = -1
-        self.offset: int = 0
-        self.suffix: str = ''
-        ServerEvent.parse(self)
-
-    def parse(self):
-        self.suffix = self.raw_string
-        match = ServerEvent.pattern.match(self.suffix)
-        if match:
-            self.timestamp = int(match.groupdict().get('timestamp', -1))
-            self.suffix = self.suffix[match.span()[1]:]
-            self.offset += match.span()[1]
+    ticks: int = 0
+    raw_string: str = ''
+    worker: ServerWorkerProxy = None
+    stream: int = -2
 
 
+@dataclass(frozen=True)
 class SessionCompleted(ServerEvent):
-
-    pattern = re.compile(r'Session completed: (?P<session_type>[a-zA-Z]+)/<session completed> with'
-                         r' (?P<car_count>\d+) cars and (?P<connection_count>\d+) connections')
-
-    def __init__(self,
-                 raw_string: str = None,
-                 worker: ServerWorkerProxy = None,
-                 out_stream_type: int = subprocess.STDOUT):
-        super(SessionCompleted, self).__init__(raw_string, worker, out_stream_type)
-        self.car_count = -1
-        self.connection_count = -1
-        self.session_type = ''
-        SessionCompleted.parse(self)
-
-    def parse(self):
-        match = SessionCompleted.pattern.match(self.suffix)
-        if match:
-            groups = match.groupdict()
-            self.session_type = groups.get('session_type', '')
-            self.car_count, ok = try_parse_int(groups.get('car_count'), -1)
-            self.connection_count, ok = try_parse_int(groups.get('connection_count', ''), -1)
-            self.suffix = self.suffix[match.span()[1]:]
-            self.offset += match.span()[1]
+    car_count: int = 0
+    connection_count: int = 0
+    session_type: str = ''
 
 
+@dataclass(frozen=True)
 class SessionPhaseChanged(ServerEvent):
+    session_type: str = ''
+    this_phase: str = ''
+    next_phase: str = ''
 
-    pattern = re.compile(r'Detected\s+sessionPhase\s+<(?P<previouse_pahse>[\w\d\s]+)>\s+'
-                         r'->\s+<(?P<current_pahse>[\w\d\s]+)>\s+\((?P<session_type>[a-zA-Z]+)\)')
 
-    def __init__(self,
-                 raw_string: str = None,
-                 worker: ServerWorkerProxy = None,
-                 out_stream_type: int = subprocess.STDOUT):
-        super(SessionPhaseChanged, self).__init__(raw_string, worker, out_stream_type)
-        self.session_type: str = ''
-        self.previous_phase: str = ''
-        self.current_phase: str = ''
-        SessionPhaseChanged.parse(self)
+@dataclass(frozen=True)
+class EventEnd(ServerEvent):
+    # Has no additional fields
+    pass
 
-    def parse(self):
-        match = self.pattern.match(self.suffix)
-        if match:
-            groups = match.groupdict()
-            self.session_type = groups.get('session_type', '')
-            self.previous_phase = groups.get('previous_phase', '')
-            self.current_phase = groups.get('current_phase', '')
-            self.offset += match.span()[1]
-            self.suffix = self.suffix[match.span()[1]:]
+
+class BaseEventCreator:
+
+    def test(self, string: str) -> bool:
+        pass
+
+    def parse(self, string: str) -> ServerEvent:
+        pass
+
+
+class RegExpEventCreator(BaseEventCreator):
+
+    pattern = re.compile(r'')
+
+    event_data_class = ServerEvent
+
+    type_convert_table = {}
+
+    def test(self, string: str) -> bool:
+        match = self.pattern.match(string)
+        return match is not None
+
+    def parse(self, string: str, **additional_params) -> event_data_class:
+        match = self.pattern.match(string)
+        groups = match.groupdict()
+        for key, value in groups.items():
+            if key in self.type_convert_table:
+                groups[key] = self.type_convert_table[key](value)
+        groups.update(additional_params)
+        return self.event_data_class(**groups)
+
+
+class EventCreators:
+
+    class SessionComplete(RegExpEventCreator):
+        pattern = re.compile(r'(?P<timestamp>\d+):\s+Session completed: (?P<session_type>[a-zA-Z]+)'
+                             r'/<session completed> with (?P<car_count>\d+) cars and (?P<connection_count>\d+) connections')
+
+        event_data_class = SessionCompleted
+
+        type_convert_table = {
+            'timestamp': int,
+            'car_count': int,
+            'connection_count': int
+        }
+
+    class SessionPhaseChanged(RegExpEventCreator):
+        pattern = re.compile(r'(?P<timestamp>\d+):\s+Detected\s+sessionPhase\s+<(?P<this_phase>[\w\d\s]+)>\s+'
+                             r'->\s+<(?P<next_phase>[\w\d\s]+)>\s+\((?P<session_type>[a-zA-Z]+)\)')
+
+        event_data_class = SessionPhaseChanged
+
+        type_convert_table = {
+            'timestamp': int
+        }
+
+    class EventEnd(RegExpEventCreator):
+        pattern = re.compile(r'(?P<timestamp>\d+):.+Weekend reset,.+')
+
+        event_data_class = EventEnd
+
+        type_convert_table = {
+            'timestamp': int
+        }
