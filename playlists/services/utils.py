515b029762e52b23fe0970c6f90610f2b9560e4c
Артём Буренин <burenotti@gmail.com>
2021-05-22 14:08:46 +0300

    Добавлена регистрация на ивенты

--- playlists/services/utils.py
+++ playlists/services/utils.py
@@ -11,16 +11,11 @@ def get_recent_events(limit: int = 50, offset: int = 0, user: User = None, verif
     return events.order_by('starts_at')[offset: offset + limit]
 
 
-
 def get_event_if_available(user: User, event_id: int):
     event_list = Event.objects.filter(id=event_id)
     if len(event_list):
         event = event_list[0]
-        if event.playlist.hidden:
-            if user.is_authenticated and user.id == event.playlist.creator:
-                return event
-        else:
-            return event
+        return event
     return None
 
 
--- playlists/urls.py
+++ playlists/urls.py
@@ -1,5 +1,5 @@
-from .views import event, get_events, create_playlist, get_playlist, register_on_event, unregister_on_event, \
-    create_event, get_results, indev, get_playlists, get_playlist_races, register_on_playlist, tools, create_report, reports, about_us
+from .views import event, get_events, get_playlist, register_on_event, unregister_on_event, \
+    create_event, get_results, indev, get_playlists, tools, create_report, reports
 from django.urls import path
 
 urlpatterns = [
@@ -18,8 +18,8 @@ urlpatterns = [
     path('event/<int:event_id>/register', register_on_event),
     path('event/<int:event_id>/unregister', unregister_on_event),
     path('championships', get_playlists),
-    path('playlist/create', create_playlist),
-    path('playlist/<int:playlist_id>', get_playlist),
-    path('playlist/<int:playlist_id>/register', register_on_playlist),
-    path('playlist/<int:playlist_id>/races', get_playlist_races)
+    # path('playlist/create', create_playlist),
+    # path('playlist/<int:playlist_id>', get_playlist),
+    # path('playlist/<int:playlist_id>/register', register_on_playlist),
+    # path('playlist/<int:playlist_id>/races', get_playlist_races)
 ]
